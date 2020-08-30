6e43c40d116ea903b0fd7969cd65873802b16f75
Alexander Launi <alex.launi@gmail.com>
2020-08-30 15:09:12 -0400

    force push instead of incremental update since public is not tracked

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,4 @@
+// Party or death mode
 (function() {
     const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
@@ -20,4 +21,22 @@
             toggleSwitch.checked = true;
         }
     }
-})()
+})();
+
+//Dog image hover
+(function() {
+    const images = document.querySelectorAll('.image-rollover');
+    images.forEach(dog => {
+        dog.addEventListener('pointerover', (image) => {
+            console.log(image)
+            let photo =  document.getElementById('photo')
+            photo.src = `/images/${image.srcElement.dataset.filename}`
+        }, false)
+    })
+
+    const doglist = document.querySelector('.doglist')
+    doglist.addEventListener('pointerleave', () => {
+        let photo =  document.getElementById('photo')
+        photo.src = `/images/selfdeath.png`
+    }, false)
+})();
