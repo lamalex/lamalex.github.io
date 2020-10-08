1351996ae2433fda69d3ac1fcae8d61ec025e026
Alexander Launi <alex.launi@gmail.com>
2020-10-08 14:51:26 -0400

    functionize spellcheck

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+function spellcheck_content() {
+    for f in content/**/*.md; do
+        echo $f:;
+        aspell -M list < $f | while read word;
+            do echo $word
+            grep -n $word $f;
+        done
+        echo '';
+    done
+}
 
-for f in content/**/*.md; do 
-    echo $f:; 
-    aspell -M list < $f | while read word; 
-        do echo $word
-        grep -n $word $f;
-    done 
-    echo '';
-done
+echo "$(spellcheck)"
