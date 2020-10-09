1f37d54480235b7f11e16e504791298ffbdf37ab
Alexander Launi <alex.launi@gmail.com>
2020-10-08 20:16:17 -0400

    Fix custom dictionary usage

--- spellcheck.sh
+++ spellcheck.sh
@@ -3,7 +3,7 @@
 function spellcheck_content() {
     for f in content/**/*.md; do
         echo '####' $f:;
-        aspell -M --ignore 2 --add-extra-dicts=custom-dictionary.txt list < $f | while read word;
+        aspell -M --ignore 2 --add-extra-dicts=./custom-dictionary.txt list < $f | while read word;
             do echo **$word**
             grep -n $word $f;
         done
